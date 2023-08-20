e29895fc7f96bf667557ee97fd6a9a3de51fc6a2
jonaprojects <merkava234@gmail.com>
2023-08-20 23:01:56 +0300

    This is the template, go back here in case something goes wrong

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-import setuptools
+import pathlib
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-setuptools.setup(
-    name="assembly-emu",
-    version="0.0.1",
-    author="jonaprojects",
-    author_email="merkava234@gmail.com",
-    description="A basic emulator of asm8086 in python",
-    long_description=long_description,
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="assembly emulator",
+    version="1.0.3",
+    description="Emulate basic Lisp environment in python for educational reasons.",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/jonaprojects/assemblyEmulator",
+    author="jonaprojects",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6"
+    packages=["assembly_emulator"],
+    include_package_data=True,
+    install_requires=["pyfiglet"],
 )
